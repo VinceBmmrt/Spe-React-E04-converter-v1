4e30a471f0728a9843afa2ddee8a6eb70a294cf3
VinceBmmrt <vincebommert75@gmail.com>
2023-12-27 17:46:37 +0100

    commit

---
+++ src/components/BaseAmount/BaseAmount.tsx
@@ -0,0 +1,12 @@
+import './BaseAmount.scss';
+
+function BaseAmount() {
+  return (
+    <header className="base-amount">
+      <h1 className="base-amount__title">Amount</h1>
+      <p className="base-amount__value">1 €</p>
+    </header>
+  );
+}
+
+export default BaseAmount;
